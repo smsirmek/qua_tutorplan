13577377a68db1a8eaab4b2f9e2bf07fb129e705
DTSIRIN <boat_num@hotmail.com>
2021-10-21 13:09:42 +0700

    del tab home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -2,21 +2,7 @@
 
   <q-page class="flex q-pa-md">
     <q-card class="full-width">
-      <q-tabs
-        v-model="$options"
-        dense
-        class="text-grey"
-        active-color="primary"
-        indicator-color="primary"
-        align="justify"
-        narrow-indicator
-      >
-        <q-tab name="work" label="Work" />
-        <q-tab name="listPage" label="Student Data" />
-        <q-tab name="map" label="Map" />
-        <q-tab name="transaction" label="Transaction" />
-        <q-tab name="profile" label="Profile"/>
-      </q-tabs>
+      today
     </q-card>
   </q-page>
 </template>
