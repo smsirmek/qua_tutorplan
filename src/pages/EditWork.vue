f6aa42becab7ce7806b4aeeaafb5ce40d33d2bfc
DTSIRIN <boat_num@hotmail.com>
2021-11-14 23:41:32 +0700

    editwork redo

--- src/pages/EditWork.vue
+++ src/pages/EditWork.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="q-pa-md doc-container">
-      <q-banner class="bg-grey-3 rounded-borders">
+    <q-banner class="bg-grey-3 rounded-borders">
     <q-btn
       class="glossy"
       round
@@ -8,7 +8,7 @@
       icon="arrow_back"
       @click="backToHome"
     />
-    <p class="q-px-md q-pt-md flex flex-center " >Edit Work</p>
+    <p class="q-px-md q-pt-md flex flex-center " >Add Work</p>
     <p class="q-px-sm q-pt-sm">Title</p>
     <q-input v-model="title" outlined type="title" class="q-px-md q-pd-md" />
     <p class="q-px-sm q-pt-md">Name</p>
@@ -17,9 +17,11 @@
       <div>
         <p class="q-px-sm q-pt-md">Date</p>
       </div>
-      <q-input v-model="date" filled type="date" hint="Native date" />
+      <div class="q-px-md q-pt-sm">
+      <q-date v-model="date" filled type="date" hint="Native date"/>
+      </div>
     </div>
-    <div class="row">
+    <div class="row q-pt-md">
       <div class="q-px-sm q-pt-md">Time</div>
       <div>
         <q-input
@@ -66,8 +68,6 @@
         </q-input>
       </div>
     </div>
-    <p class="q-px-sm q-pt-sm">Alert</p>
-          <q-select class="q-px-md q-mt-md" outlined v-model="alert" :options="options" />
     <p class="q-px-sm q-pt-md">Details</p>
           <q-input v-model="details" outlined type="textarea" class="q-px-md q-pd-md" />
     <div class="row q-pt-md">
@@ -93,10 +93,10 @@
           type="submit"
           color="primary"
           label="Edit"
-          @click="editWork"
+          @click="setUpLogic"
         />
     </div>
-</q-banner>
+    </q-banner>
   </div>
 </template>
 
@@ -105,11 +105,12 @@ import moment from 'moment'
 import firebase from 'firebase'
 import { useQuasar } from 'quasar'
 const db = firebase.firestore()
+let User
 export default {
   data () {
     return {
       title: null,
-      date: null,
+      date: [],
       beginingTime: null,
       endingTime: null,
       serviceCharge: null,
@@ -121,7 +122,8 @@ export default {
       options: [
         'On', 'Off'
       ],
-      quasarPlugin: useQuasar()
+      quasarPlugin: useQuasar(),
+      Check: []
     }
   },
   methods: {
@@ -139,7 +141,6 @@ export default {
           this.serviceCharge = doc.data().Amountperhour
           this.details = doc.data().Details
           this.name = doc.data().Name
-          this.alert = doc.data().Alert
           this.timeCalculator = doc.data().Timeinminutes
         }).catch((err) => { console.log(err) })
       console.log(this.name)
@@ -147,13 +148,11 @@ export default {
     },
     async editWork () {
       const User = await firebase.getCurrentUser()
-      if (this.title && this.name && this.date && this.timeCalculator && this.alert && this.details && this.serviceCharge && this.totalServicecharge && this.beginingTime && this.endingTime) {
         await db.collection('WorkList').doc(this.$route.params.Key).update({
           Title: this.title,
           Name: this.name,
           Date: this.date,
           Timeinminutes: this.timeCalculator,
-          Alert: this.alert,
           Details: this.details,
           Amountperhour: this.serviceCharge,
           Totalservicecharge: this.totalServicecharge,
@@ -161,15 +160,86 @@ export default {
           EndingTime: this.endingTime,
           userId: User.uid
         }).catch((err) => { console.log(err) })
-        await this.updateStudentDebt()
-        this.$router.push('/home')
+    },
+    async queryDate () {
+      await db.collection('WorkList').where('userId', '==', User.uid).get()
+      .then((doc) => {
+        doc.forEach((doc) => {
+          const obj = {}
+        obj.beginTime  = moment(doc.data().Date).format('YYYY-MM-DD').concat(' '+ doc.data().BeginingTime)
+        obj.endTime = moment(doc.data().Date).format('YYYY-MM-DD').concat(' '+ doc.data().EndingTime)
+          this.Check.push(obj)
+        })
+      })
+    }, 
+    async checkDateTime () {
+      {
+      for(let i=0; i<this.Check.length; i++){
+          for(let j=0; j<this.date.length; j++){
+            const timeAddBegin = moment(this.date[j]).format('YYYY-MM-DD').concat(' '+this.beginingTime)
+            const timeAddEnd = moment(this.date[j]).format('YYYY-MM-DD').concat(' '+this.endingTime)
+            const checkTimeBetween = moment(timeAddBegin).isBetween(this.Check[i].beginTime, this.Check[i].endTime)
+            const checkTimeBetweenEnd = moment(timeAddEnd).isBetween(this.Check[i].beginTime, this.Check[i].endTime)
+            const checkBeginTime = moment(timeAddBegin).isSame(moment(this.Check[i].beginTime))
+            const checkEndTime = moment(timeAddEnd).isSame(moment(this.Check[i].endTime))
+            const checkQueryTimeBetween = moment(this.Check[i].beginTime).isBetween(timeAddBegin, timeAddEnd)
+            const log = console.log
+            
+            if(checkTimeBetween || checkBeginTime || checkEndTime || checkTimeBetweenEnd || checkQueryTimeBetween){
+              this.quasarPlugin.notify({message: `Schedule conflict`, color: 'red'})
+              return true
+            }else{
+              }
+          }          
+      }
+      return false
+      }
+    },
+    async setUpLogic () {
+      const checkDateTime = await this.checkDateTime()
+      const checkValidate = await this.inputValidate()
+      const log = console.log
+      if(checkValidate && !checkDateTime || checkDateTime === undefined){
+      // log(checkDateTime)
+      // log(checkValidate)
+         await  this.editWork()
+         await this.updateStudentDebt()
+        await this.createStudentBill()
+       this.$router.push('/home')
+      }
+    },
+    async inputValidate (){
+      if (this.title && this.name && this.date && this.timeCalculator  && this.details && this.serviceCharge && this.totalServicecharge && this.beginingTime && this.endingTime){
+        return true
+      }else{
+        return false
       }
     },
     async updateStudentDebt () {
-      console.log(this.name.value)
       await db.collection('StudentList').doc(this.name.value).update({
-        debt: this.totalServicecharge
-      }).catch((err) => { console.log(err) })
+        debt: this.name.Debt + this.totalServicecharge
+      }).catch((err) => { console.log(err) }).then(() => { console.log('update success') })
+    },
+    async createStudentBill () {
+      const m = moment(this.endingTime, 'h:mm').diff(moment(this.beginingTime, 'h:mm'), 'minutes')
+      const hours = Math.floor(m / 60)
+      const minutes = m % 60
+      let prepare
+      if (minutes !== 0) {
+        let x = + minutes * 0.01
+        prepare = hours + x
+      } else {
+        prepare = hours
+      }
+      await db.collection('Bill').add({
+        Date: moment(this.date).format('DD/MM/YYYY'),
+        Hour: prepare,
+        Total: this.totalServicecharge,
+        paid: false,
+        studentName: this.name.label,
+        userId: User.uid
+      })
+
     },
     showLoading (isLoading) {
       if (isLoading) {
@@ -177,13 +247,14 @@ export default {
       } else if (!isLoading) {
         this.quasarPlugin.loading.hide()
       }
-    }
+    },
   },
   computed: {
     totalServicecharge () {
       let sum = 0
       let excessTime = 0
       let finalTime = 0
+      if(moment(this.beginingTime, 'h:mm').isBefore(moment(this.endingTime,'h:mm'))){
       const totalTime = moment(this.endingTime, 'h:mm').diff(moment(this.beginingTime, 'h:mm'), 'minutes')
       if (totalTime % 60 !== 0) {
         excessTime = totalTime % 60
@@ -193,10 +264,12 @@ export default {
         sum = (totalTime / 60) * this.serviceCharge
       }
       return sum
+    }
     },
     timeCalculator () {
       let excessTime = 0
       let finalTime = 0
+      if(moment(this.beginingTime, 'h:mm').isBefore(moment(this.endingTime, 'h:mm'))){
       const totalTime = moment(this.endingTime, 'h:mm').diff(moment(this.beginingTime, 'h:mm'), 'minutes')
       if (totalTime % 60 !== 0) {
         excessTime = totalTime % 60
@@ -205,11 +278,18 @@ export default {
         finalTime = totalTime
       }
       return finalTime
+      }else{
+        this.quasarPlugin.notify(
+          {message: 'Please select new time', color: 'red'}
+          )
+       }
     }
 
   },
   async created () {
+    User = await firebase.getCurrentUser()
     await this.queryWork()
+    await this.queryDate()
   }
 }
 </script>
